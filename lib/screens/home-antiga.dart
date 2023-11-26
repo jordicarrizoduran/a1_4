ef9e3c5ce85e2dea17f7ec1a356d3402ef0b1135
jordicarrizoduran <jordicarrizoduran@gmail.com>
2023-11-26 11:15:47 +0100

    pugem nova versió home

---
+++ lib/screens/home-antiga.dart
@@ -0,0 +1,69 @@
+import 'package:a1_4/models/postal_codes.dart';
+import 'package:a1_4/services/postal_service.dart';
+import 'package:flutter/material.dart';
+
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final myController = TextEditingController();
+  late CodiPostals postalCodes = CodiPostals(postCode: '', country: '', countryAbbreviation: '', places: []);
+
+  @override
+  void dispose(){
+    myController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Codi Postals"),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              children: [
+                const Text(
+                    'Escriu un codi postal i prem a "Buscar"'),
+                const SizedBox(height: 20),
+                TextField(
+                  controller: myController,
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () async {
+                    CodiPostals result = await PostalService().fetchData(myController.text);
+                    // Actualitzar la UI o fer altres operacions amb postalCodes
+                    setState(() {
+                      postalCodes = result;
+                    });
+                  },
+                  child: const Text('Buscar'),
+                ),
+                const SizedBox(height: 40),
+                Row(
+                  children: [
+                    if(postalCodes != null && postalCodes.postCode != null) ...[
+                      Text(postalCodes.postCode),
+                      Text(postalCodes.countryAbbreviation),
+                    ] else
+                      const Text('Codi postal no guardat')
+
+                  ],
+                ),
+              ],
+            )
+
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,9 +2,8 @@ import 'package:a1_4/models/postal_codes.dart';
 import 'package:a1_4/services/postal_service.dart';
 import 'package:flutter/material.dart';
 
-
 class Home extends StatefulWidget {
-  const Home({super.key});
+  const Home({Key? key}) : super(key: key);
 
   @override
   State<Home> createState() => _HomeState();
@@ -12,69 +11,81 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final myController = TextEditingController();
-  late CodiPostals postalCodes = CodiPostals(
-      postCode: '', country: '', countryAbbreviation: '', places: []);
+  late CodiPostals postalCodes = CodiPostals(postCode: '', country: '', countryAbbreviation: '', places: []);
+  final _formKey = GlobalKey<FormState>();
+  bool _buttonPressed = false; // Variable per indicar si s'ha premut el botó o no
 
   @override
-  void dispose(){
+  void dispose() {
     myController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: const Text("Codi Postals"),
       ),
       body: SingleChildScrollView(
         child: Padding(
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                children: [
-                  const Text(
-                      'Escriu un codi postal i prem a "Buscar"'),
-                  const SizedBox(height: 20),
-                  TextField(
+          padding: const EdgeInsets.all(16),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                const Text('Escriu un codi postal i prem a "Buscar"'),
+                const SizedBox(height: 20),
+                TextFormField(
                   controller: myController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Introdueix un codi postal';
+                    }
+                    return null;
+                  },
                 ),
-                  const SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () async {
-                      try {
-                        // Fer la crida a fetchData
-                        CodiPostals result = await PostalService().fetchData(myController.text);
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () async {
+                    // Validar el formulari abans de continuar
+                    if (_formKey.currentState?.validate() ?? false) {
+                      // Només fer la crida a fetchData quan l'usuari prem el botó
+                      setState(() {
+                        _buttonPressed = true;
+                      });
+                    }
+                  },
+                  child: const Text('Buscar'),
+                ),
+                FutureBuilder(
+                  future: _buttonPressed ? PostalService().fetchData(myController.text) : null,
+                  builder: (context, snapshot) {
+                    if (!_buttonPressed) {
+                      return const SizedBox.shrink(); // No mostrar res fins que l'usuari premi el botó
+                    } else if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Text('Carregant');
+                    } else if (snapshot.hasError) {
+                      return const Text('Aquest codi postal no està registrat');
+                    } else if (snapshot.hasData) {
+                      var codiPostal = snapshot.data as CodiPostals;
 
-                        // Actualitzar la UI o fer altres operacions amb postalCodes
-                        setState(() {
-                          postalCodes = result;
-                        });
-
-                        debugPrint(postalCodes.postCode);
-                      } catch (e) {
-                        // Gestionar errors, com mostrar un missatge d'error
-                        debugPrint("Error: $e");
-                      }
-                    },
-                    child: const Text('Buscar'),
-                  ),
-                  const SizedBox(height: 40),
-                  Row(
-                    children: [
-                      if (postalCodes != null && postalCodes.postCode != null)
-                        ...[
-                          Text(postalCodes.postCode),
-                          Text(postalCodes.countryAbbreviation ?? ''),
-                        ]
-                      else
-                        Text("Aquest codi postal no existeix"),
-                    ],
-                  ),
-                ],
-              )
-
+                      return Column(
+                        children: [
+                          Text(codiPostal.postCode),
+                          Text(codiPostal.countryAbbreviation),
+                        ],
+                      );
+                    } else {
+                      return const SizedBox.shrink();
+                    }
+                  },
+                ),
+                const SizedBox(height: 40),
+              ],
+            ),
           ),
+        ),
       ),
     );
   }
