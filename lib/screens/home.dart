e7e046cebeecb9919c539ca6924bbbfe034ec197
jordicarrizoduran <jordicarrizoduran@gmail.com>
2023-11-26 13:36:40 +0100

    afegim Maps a la poblacio

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,8 @@
 import 'package:a1_4/models/postal_codes.dart';
 import 'package:a1_4/services/postal_service.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -14,6 +16,7 @@ class _HomeState extends State<Home> {
   late CodiPostals postalCodes = CodiPostals(postCode: '', country: '', countryAbbreviation: '', places: []);
   final _formKey = GlobalKey<FormState>();
   bool _buttonPressed = false; // Variable per indicar si s'ha premut el botó o no
+
 
   @override
   void dispose() {
@@ -74,15 +77,28 @@ class _HomeState extends State<Home> {
                       return Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          const Text('Codi Postal:', style: TextStyle(fontWeight:FontWeight.bold),),
+                          const Text('Codi Postal:', style: TextStyle(fontWeight: FontWeight.bold)),
                           Text(codiPostal.postCode),
                           const SizedBox(height: 20),
-                          const Text('Poblacions:', style: TextStyle(fontWeight:FontWeight.bold),),
-                          ...codiPostal.places.map((place) => Text(place.placeName)),
+                          const Text('Poblacions (clica a sobre per anar a Maps):', style: TextStyle(fontWeight: FontWeight.bold)),
+                          ...codiPostal.places.map((place) => InkWell(
+                            onTap: () {
+                              _launchUrl(place.latitude, place.longitude);
+                            },
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(place.placeName),
+                                const SizedBox(width: 8),
+                                const Icon(Icons.location_on),
+                                const Text('(Google Maps)'),
+                              ],
+                            ),
+                          )),
                         ],
                       );
                     } else {
-                      return const Text('Carregant');
+                      return const CircularProgressIndicator();
                     }
                   },
                 ),
@@ -93,5 +109,13 @@ class _HomeState extends State<Home> {
         ),
       ),
     );
+  }
+  // Funció per obrir Google Maps amb les coordenades específiques.
+  Future<void> _launchUrl(String latitude, String longitude) async {
+    final Uri url = Uri.parse('https://www.google.com/maps/search/?api=1&query=$latitude,$longitude');
+
+    if (!await launchUrl(url)) {
+      throw Exception('Could not launch $url');
+    }
   }
 }
